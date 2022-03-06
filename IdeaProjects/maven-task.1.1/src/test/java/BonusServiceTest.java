24fccaf617edd59038f4e4869f3137a27208a938
EvgenyKatukhov <96973714+EvgenyKatukhov@users.noreply.github.com>
2022-03-06 15:08:36 +0300

    Update BonusServiceTest.java

--- IdeaProjects/maven-task.1.1/src/test/java/BonusServiceTest.java
+++ IdeaProjects/maven-task.1.1/src/test/java/BonusServiceTest.java
@@ -33,4 +33,33 @@ public class BonusServiceTest {
         // производим проверку (сравниваем ожидаемый и фактический):
         assertEquals(expected, actual);
     }
+
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForRegistered() {
+        BonusService service = new BonusService();
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
+    @org.junit.jupiter.api.Test
+    void shouldCalculateForUnderlimit() {
+        BonusService service = new BonusService();
+        // подготавливаем данные:
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 30;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        assertEquals(expected, actual);
+    }
 }
